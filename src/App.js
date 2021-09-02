02615cf7476e0ad1e5b6464afa8789aa17607c2f
Harsh Patel <harsh.coderc@gmail.com>
2021-09-02 19:34:40 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import IndexRoutes from "./helper/routes";
 
 const App = () => {
     return (
         <Router>
             <div className="w-screen h-screen">
+                <Navbar />
                 <IndexRoutes />
             </div>
         </Router>
